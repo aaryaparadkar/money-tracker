51f2177294d48fac45fbd758f29b1318e75f6c33
Aarya Paradkar <aaru7276@gmail.com>
2024-01-08 21:39:02 +0530

    setup routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port)
 
 app.get('/api/test', (req, res) => {
-    res.send('test ok')
+    res.json('test ok')
+})
+
+app.post('/api/transaction', (req, res) => {
+    res.json(req.body)
 })
